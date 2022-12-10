3b397c58d33d028c753e970066d36dceb2d8ec5b
Alexandre <almmello@gmail.com>
2022-12-09 21:48:34 -0300

    Feito!

--- utils/mdx-utils.js
+++ utils/mdx-utils.js
@@ -15,5 +15,12 @@ export const getPostBySlug = async (id) => {
     //TODO: BUSCAR UM POST EM ESPECIFICO.
     //const {data} = await api.get(`/post?id=eq.${id}`)
 
-    return {}
+  // Search for a specific post.
+  const { data } = await api.get(`/posts?id=eq.${id}`);
+  if (data && data.length > 0) {
+    return data[0];
+  } else {
+    return {};
+  }
+
 }
